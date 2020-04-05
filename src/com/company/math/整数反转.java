582a818317a396ab809ed2d81cb5e0c21980f6d2
gaoyuxuan <sheen_gyx@163.com>
2020-04-06 01:57:07 +0800

    2020 04.05 23:00

--- src/com/company/math/整数反转.java
+++ src/com/company/math/整数反转.java
@@ -3,26 +3,39 @@ package com.company.math;
 public class 整数反转 {
     public static void main(String[] args) {
         整数反转 s = new 整数反转();
-        System.out.println(s.reverse(-123011));
+        System.out.println(s.reverse(Integer.MIN_VALUE));
     }
 
     public int reverse(int x) {
-        int flag = 0;
-        if(x/Math.pow(10,9)!=0) {
-            if(Math.abs(x%10)>2) return 0;
-            else {
-                flag = Math.abs(x%10);
-                x /= 10;
+        int max = 1000000000;
+        if(x/max!=0) {
+            boolean fuhao = x>0?true:false;
+            if(x%10>2||x%10<-2) return 0;
+            else if(x%10==2||x%10==-2) {
+                int m = x/10;
+                int res = 0;
+                if(fuhao) {
+                    res = reverse(m);
+                    if(res>(Integer.MAX_VALUE%max)) return 0;
+                }else {
+                    res = reverse(m);
+                    if(res<(Integer.MIN_VALUE%max)) return 0;
+                }
+                return fuhao?2*max+res:-2*max+res;
             }
         }
+        int index = 1;
+        int num = 10;
+        while(x/num!=0) {
+            index++;
+            num*=10;
+        }
         int res = 0;
-        do {
-            int tmp = x%10;
-            res*=10;
-            res = res+tmp;
-            x = x/10;
-        } while (x!=0);
-        if(flag==0) return res;
+        for(int i= index;i>0;i--) {
+            res += x%10;
+            x/=10;
+            if(i!=1) res*=10;
+        }
         return res;
     }
 }
--- src/com/company/util/Test.java
+++ src/com/company/util/Test.java
@@ -1,5 +1,7 @@
 package com.company.util;
 
+import com.company.unknow.LFUCache;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -10,7 +12,7 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        List<Person> list = new ArrayList<>();
+        /*List<Person> list = new ArrayList<>();
         list.stream().collect(Collectors.toMap(x -> x.sex, x->{
             List<String> l = new ArrayList<>();
             l.add(x.name);
@@ -18,9 +20,22 @@ public class Test {
         }, (l, x)->{
             l.addAll(x);
             return l;
-        }));
-
-
+        }));*/
+        LFUCache cache = new LFUCache(3);
+        cache.put(1,1);
+        cache.put(2,2);
+        cache.put(3,3);
+        cache.put(4,4);
+        System.out.println(cache.get(4));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(2));
+        System.out.println(cache.get(1));
+        cache.put(5,5);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(2));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
+        System.out.println(cache.get(5));
     }
 
     public class Person{
