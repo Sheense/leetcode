68fbc6fea5b85a00a74206a5f96976c0e1abab3c
gaoyuxuan <sheen_gyx@163.com>
2020-04-16 01:54:10 +0800

    2020 04.15 23:00

---
+++ src/com/company/stack/柱状图中最大的矩形.java
@@ -0,0 +1,10 @@
+package com.company.stack;
+
+/*
+单调栈
+ */
+public class 柱状图中最大的矩形 {
+    public int largestRectangleArea(int[] heights) {
+        return 1;
+    }
+}
---
+++ src/com/company/violence/柱状图中最大的矩形.java
@@ -0,0 +1,34 @@
+package com.company.violence;
+
+public class 柱状图中最大的矩形 {
+
+    public static void main(String[] args) {
+        柱状图中最大的矩形 s = new 柱状图中最大的矩形();
+        System.out.println(s.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        if(heights.length==0) return 0;
+        int max =0;
+        for(int i=0;i<heights.length;i++) {
+            int left = i;
+            int right = i;
+            boolean flag1 = true;
+            boolean flag2 = true;
+            while(flag1||flag2) {
+                if(left-1>=0&&heights[left-1]>=heights[i]) {
+                    left--;
+                }else {
+                    flag1 = false;
+                }
+                if(right+1<heights.length&&heights[right+1]>=heights[i]) {
+                    right++;
+                }else {
+                    flag2 = false;
+                }
+            }
+            max = max>heights[i]*(right-left+1)?max:heights[i]*(right-left+1);
+        }
+        return max;
+    }
+}
