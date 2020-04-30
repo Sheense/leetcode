33873e084524b4d1f48836a0fbf3beddec24eba6
gaoyuxuan <sheen_gyx@163.com>
2020-05-01 02:55:29 +0800

    2020 04.30 23:00

--- src/com/company/util/ArrayUtils.java
+++ src/com/company/util/ArrayUtils.java
@@ -4,7 +4,6 @@ import com.company.common.ListNode;
 import java.util.List;
 
 public class ArrayUtils {
-
     public static void print2(List<List<Integer>> array) {
         for(List<Integer> list : array){
             for(int i : list){
