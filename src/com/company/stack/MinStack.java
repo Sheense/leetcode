f4d33c69dabcfebd3ae15e51a0f9706394ead490
gaoyuxuan <sheen_gyx@163.com>
2020-05-12 00:57:27 +0800

    2020 05.11 23:00

---
+++ src/com/company/stack/MinStack.java
@@ -0,0 +1,48 @@
+package com.company.stack;
+
+import java.util.*;
+
+public class MinStack {
+
+
+    int min;
+    Stack<Integer> stack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        if(x<=min) {
+            stack.push(min);
+            min= x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if(stack.pop()==min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+    /*public static void main(String[] args) {
+        MinStack s = new MinStack();
+        s.push(-2);
+        s.push(0);
+        s.push(-3);
+        System.out.println(s.getMin());
+        s.pop();
+        System.out.println(s.top());
+        System.out.println(s.getMin());
+    }*/
