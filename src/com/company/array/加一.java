d24934f5599d44e27dc1a444727bc56992f93085
gaoyuxuan <sheen_gyx@163.com>
2020-05-22 01:06:51 +0800

    2020 05.21 23:00

---
+++ src/com/company/array/加一.java
@@ -0,0 +1,32 @@
+package com.company.array;
+
+public class 加一 {
+
+    public int[] plusOne(int[] digits) {
+        int j = 0;
+        digits[digits.length-1] = digits[digits.length-1] +1;
+        for(int i=digits.length-1;i>=0;i--) {
+            if(digits[i]+j>=10) {
+                digits[i] = (digits[i]+j)%10;
+                j = 1;
+            }else {
+                digits[i] = digits[i]+j;
+                j=0;
+                break;
+            }
+        }
+        if(j==0) {
+            return digits;
+        }else {
+            int[] res = new int[digits.length+1];
+            for(int i=0;i<res.length;i++) {
+                if(i==0) {
+                    res[i] = j;
+                }else {
+                    res[i] = digits[i-1];
+                }
+            }
+            return res;
+        }
+    }
+}
