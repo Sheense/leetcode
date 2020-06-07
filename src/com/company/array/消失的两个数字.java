2af513f748308cfb223d6d38b3604a6217a25621
gaoyuxuan <sheen_gyx@163.com>
2020-06-08 02:18:02 +0800

    2020 06.07 23:00

---
+++ src/com/company/array/消失的两个数字.java
@@ -0,0 +1,51 @@
+package com.company.array;
+
+import com.company.util.ArrayUtils;
+
+public class 消失的两个数字 {
+
+    public static void main(String[] args) {
+        消失的两个数字 s = new 消失的两个数字();
+        ArrayUtils.print(s.missingTwo(new int[]{2,3}));
+    }
+
+    public int[] missingTwo(int[] nums) {
+        int [] res = new int[2];
+        int a = 0;
+        int b = 0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]!=i+1) {
+                while(nums[i]!=i+1&&nums[i]!=0) {
+                    int item = nums[i];
+                    if(item==nums.length+1){
+                        a = item;
+                        nums[i]= 0;
+                        break;
+                    }
+                    if(item==nums.length+2) {
+                        b = item;
+                        nums[i]=0;
+                        break;
+                    }
+                    int value = nums[item-1];
+                    nums[item-1] = item;
+                    nums[i] = value;
+                }
+            }
+        }
+        int index = 0;
+        for(int i=0;i<nums.length+2;i++) {
+            if(index==2) break;
+            if(i==nums.length) {
+                if(a==0) res[index++] = nums.length+1;
+                continue;
+            }
+            if(i==nums.length+1) {
+                if(b==0) res[index++] = nums.length+2;
+                continue;
+            }
+            if(nums[i]==0) res[index++] = i+1;
+        }
+        return res;
+    }
+}
--- src/com/company/stack/柱状图中最大的矩形.java
+++ src/com/company/stack/柱状图中最大的矩形.java
@@ -5,6 +5,27 @@ package com.company.stack;
  */
 public class 柱状图中最大的矩形 {
     public int largestRectangleArea(int[] heights) {
-        return 1;
+        if(heights.length==0) return 0;
+        int max =0;
+        for(int i=0;i<heights.length;i++) {
+            int left = i;
+            int right = i;
+            boolean flag1 = true;
+            boolean flag2 = true;
+            while(flag1||flag2) {
+                if(left-1>=0&&heights[left-1]>=heights[i]) {
+                    left--;
+                }else {
+                    flag1 = false;
+                }
+                if(right+1<heights.length&&heights[right+1]>=heights[i]) {
+                    right++;
+                }else {
+                    flag2 = false;
+                }
+            }
+            max = max>heights[i]*(right-left+1)?max:heights[i]*(right-left+1);
+        }
+        return max;
     }
 }
