b4d7b2c4aae8878d6a9511010ea2f1b246694c75
gaoyuxuan <sheen_gyx@163.com>
2020-06-11 01:01:02 +0800

    2020 06.10 23:00

---
+++ src/com/company/stack/每日温度.java
@@ -0,0 +1,28 @@
+package com.company.stack;
+
+import com.company.recall.复原IP地址;
+import com.company.util.ArrayUtils;
+
+import java.util.Stack;
+
+//遍历时，维护一个单调递减的栈
+public class 每日温度 {
+
+    public static void main(String[] args) {
+        每日温度 s = new 每日温度();
+        ArrayUtils.print(s.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73}));
+    }
+
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[T.length];
+        for(int i=0;i<T.length;i++) {
+            while(!stack.isEmpty()&&T[stack.peek()]<T[i]) {
+                int j = stack.pop();
+                res[j] = i - j;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
