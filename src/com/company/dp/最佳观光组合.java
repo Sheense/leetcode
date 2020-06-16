2e4a59d2a272e5ecc9ff3c83f38d9d300f76ea9d
gaoyuxuan <sheen_gyx@163.com>
2020-06-17 00:47:38 +0800

    2020 06.16 23:00

---
+++ src/com/company/dp/最佳观光组合.java
@@ -0,0 +1,20 @@
+package com.company.dp;
+
+import java.util.Stack;
+
+public class 最佳观光组合 {
+    public int maxScoreSightseeingPair(int[] A) {
+        int[] dp = new int[A.length];
+        for(int i=0;i<A.length;i++) {
+            if(i==0) dp[0] = A[i] +i;
+            else {
+                dp[i] = Math.max(dp[i-1],A[i]+i);
+            }
+        }
+        int res = 0;
+        for(int i=1;i<A.length;i++) {
+            res = Math.max(res, A[i]-i+dp[i-1]);
+        }
+        return res;
+    }
+}
