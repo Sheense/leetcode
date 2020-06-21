8786cc9b8050541ce4be28079abb356da6a133f6
sheen <sheen_gyx@163.com>
2020-06-22 01:10:37 +0800

    2020 06.21 23:00

---
+++ src/com/company/string/模式匹配.java
@@ -0,0 +1,78 @@
+package com.company.string;
+
+public class 模式匹配 {
+
+    public static void main(String[] args) {
+        模式匹配 s = new 模式匹配();
+        System.out.println(s.patternMatching("bb","thuhrh"));
+    }
+    public boolean patternMatching(String pattern, String value) {
+        if(pattern.equals("")&&value.length()>0) return false;
+        int aNum = 0;
+        int bNum = 0;
+        for(int i=0;i<pattern.length();i++) {
+            if(pattern.charAt(i)=='a') aNum++;
+            if(pattern.charAt(i)=='b') bNum++;
+        }
+        if(value.equals("")){
+            if(aNum>0&&bNum>0)return false;
+            else return true;
+        }
+        if(aNum==0) {
+            for(int blen = 1;blen<=value.length()/bNum;blen++) {
+                String bString = "";
+                String string = value.substring(0,blen);
+                boolean res = true;
+                for(int i=1;i<=bNum;i++) {
+                    bString+=string;
+                }
+                if(res&&bString.equals(value)) return true;
+            }
+        }else {
+            for(int alen=0;alen<=value.length()/aNum;alen++) {
+                int blen = bNum==0?0:(value.length()-aNum*alen)/bNum;
+                if(blen*bNum+alen*aNum!=value.length()) continue;
+                if(blen==0&&alen==0) continue;
+                boolean res = true;
+                String aString = "";
+                String bString = "";
+                int index = 0;
+                for(int j=0;j<pattern.length();j++) {
+                    if(pattern.charAt(j)=='a'&&alen!=0) {
+                        if(index+alen<=value.length()) {
+                            String string = value.substring(index,index+alen);
+                            if(string.equals(aString)||aString.equals("")) {
+                                aString = string;
+                                index+=alen;
+                            }else {
+                                res = false;
+                                break;
+                            }
+                        }else {
+                            res = false;
+                            break;
+                        }
+                    }
+
+                    if(pattern.charAt(j)=='b'&&blen!=0) {
+                        if(index+blen<=value.length()) {
+                            String string = value.substring(index,index+blen);
+                            if(string.equals(bString)||bString.equals("")) {
+                                bString = string;
+                                index+=blen;
+                            }else {
+                                res = false;
+                                break;
+                            }
+                        }else {
+                            res = false;
+                            break;
+                        }
+                    }
+                }
+                if(res) return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/com/company/tree/二叉树中的最大路径和.java
@@ -0,0 +1,42 @@
+package com.company.tree;
+
+import com.company.common.TreeNode;
+
+public class 二叉树中的最大路径和 {
+    public static void main(String[] args) {
+        二叉树中的最大路径和 s = new 二叉树中的最大路径和();
+        TreeNode root = new TreeNode(9);
+        root.left = new TreeNode(6);
+        root.right = new TreeNode(-3);
+        root.right.left = new TreeNode(-6);
+        root.right.right = new TreeNode(2);
+        root.right.right.left = new TreeNode(2);
+        root.right.right.left.left = new TreeNode(-6);
+        root.right.right.left.right = new TreeNode(-6);
+        root.right.right.left.left.left = new TreeNode(-6);
+        System.out.println(s.calculate(root));
+    }
+    public int maxPathSum(TreeNode root) {
+        return calculate(root);
+    }
+
+    public int calculate(TreeNode root) {
+        if(root.left==null&&root.right==null) {
+            return root.val;
+        }
+        int left = Integer.MIN_VALUE;
+        if(root.left!=null) {
+            left = calculate(root.left);
+        }
+        int right = Integer.MIN_VALUE;
+        if(root.right!=null) {
+            right = calculate(root.right);
+        }
+        int res = root.val;
+        root.val = Math.max(root.val,root.val+Math.max((root.left==null?0:root.left.val),(root.right==null?0:root.right.val)));
+        if(root.right!=null&&root.right.val>=0) res+=root.right.val;
+        if(root.left!=null&&root.left.val>=0) res+=root.left.val;
+        return Math.max(res, Math.max(left, right));
+    }
+
+}
