7f4d839e6df9b8c3f033f53e5f0d6ddf6af4e952
gaoyuxuan <sheen_gyx@163.com>
2020-06-30 01:24:55 +0800

    2020 06.29 23:00

---
+++ src/com/company/stack/用两个栈实现队列.java
@@ -0,0 +1,29 @@
+package com.company.stack;
+
+import java.util.Stack;
+
+public class 用两个栈实现队列 {
+    class CQueue {
+
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if(stack1.empty()&&stack2.empty()) return -1;
+            else if(stack2.empty()) {
+                while(!stack1.empty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
+        }
+    }
+}
