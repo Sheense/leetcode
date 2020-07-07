ff0be40eaabe1d2f035b22edb786ea614dd94080
gaoyuxuan <sheen_gyx@163.com>
2020-07-08 01:24:33 +0800

    2020 07.07 23:00

---
+++ src/com/company/math/跳水板.java
@@ -0,0 +1,29 @@
+package com.company.math;
+
+import com.company.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 跳水板 {
+    public static void main(String[] args) {
+        跳水板 s = new 跳水板();
+        ArrayUtils.print(s.divingBoard(1,2,3));
+    }
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if(k==0) return new int[]{};
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<=k;i++) {
+            int num = (k-i)*shorter+i*longer;
+            if(list.isEmpty()||list.get(list.size()-1)<num)
+            list.add(num);
+        }
+        int[] res = new int[list.size()];
+        for(int i=0;i<res.length;i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+
+}
