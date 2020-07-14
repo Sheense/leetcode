6d69e97f1c96c0bfa9adb16b6a0440245edf12b3
gaoyuxuan <sheen_gyx@163.com>
2020-07-15 01:07:20 +0800

    2020 07.14 23:00

--- src/com/company/tree/不同的二叉搜索树.java
+++ src/com/company/tree/不同的二叉搜索树.java
@@ -7,20 +7,21 @@ import java.util.List;
 
 public class 不同的二叉搜索树 {
 
+    public static void main(String[] args) {
+        不同的二叉搜索树 s = new 不同的二叉搜索树();
+        System.out.println(s.numTrees(3));
+    }
+
     public int numTrees(int n) {
         if(n==0) return 0;
-        return build(0,n-1);
-    }
-    public int build(int start, int end) {
-        if(start>=end) {
-            return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i=1;i<n+1;i++) {
+            int v = i-1;
+            for(int j=0;j<=v;j++) {
+                dp[i] += dp[j] * dp[v-j];
+            }
         }
-        int res = 0;
-        for(int i=start;i<=end;i++) {
-            int left = build(start, i-1);
-            int right = build(i+1,end);
-            res += left*right;
-        }
-        return res;
+        return dp[n];
     }
 }
---
+++ src/com/company/tree/二叉搜索树迭代器.java
@@ -0,0 +1,37 @@
+package com.company.tree;
+
+import com.company.common.TreeNode;
+
+
+import java.util.Stack;
+
+public class 二叉搜索树迭代器 {
+    class BSTIterator {
+
+        private Stack<TreeNode> stack;
+        public BSTIterator(TreeNode root) {
+            stack = new Stack<>();
+            TreeNode node =root;
+            while(node!=null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        /** @return the next smallest number */
+        public int next() {
+            TreeNode node = stack.pop();
+            TreeNode n = node.right;
+            while(n!=null) {
+                stack.push(n);
+                n = n.left;
+            }
+            return node.val;
+        }
+
+        /** @return whether we have a next smallest number */
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    }
+}
