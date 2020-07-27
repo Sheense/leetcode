bb9c2b84fd47d4135dad9b57a70a159161cc0a3c
sheen <sheen_gyx@163.com>
2020-07-28 00:18:18 +0800

    2020 07.26 23:00

--- src/com/company/dp/矩阵中的最长递增路径.java
+++ src/com/company/dp/矩阵中的最长递增路径.java
@@ -13,51 +13,51 @@ public class 矩阵中的最长递增路径 {
     }
     public int longestIncreasingPath(int[][] matrix) {
         if(matrix.length==0) return 0;
-        int res = 1;
         int [][] dp = new int [matrix.length][matrix[0].length];
         for(int i=0;i<dp.length;i++) {
             for(int j=0;j<dp[0].length;j++) dp[i][j] = 1;
         }
+        int res = 0;
         for(int i=0;i<matrix.length;i++) {
             for(int j=0;j<matrix[0].length;j++) {
-                int max = 0;
-                if(i-1>=0) {
-                    if(matrix[i-1][j]<matrix[i][j]) max = Math.max(max, dp[i-1][j]);
+                if(dp[i][j]==1) {
+                    recall(dp,matrix,i,j);
                 }
-                if(i+1<matrix.length) {
-                    if(matrix[i+1][j]<matrix[i][j]) max = Math.max(max, dp[i+1][j]);
-                }
-                if(j-1>=0) {
-                    if(matrix[i][j-1]<matrix[i][j]) max = Math.max(max, dp[i][j-1]);
-                }
-                if(j+1<matrix[0].length) {
-                    if(matrix[i][j+1]<matrix[i][j]) max = Math.max(max, dp[i][j+1]);
-                }
-                dp[i][j] = max+1;
-                if(res<dp[i][j]) res = dp[i][j];
             }
         }
-
-        for(int i=matrix.length-1;i>=0;i--) {
-            for(int j=matrix[0].length-1;j>=0;j--) {
-                int max = 0;
-                if(i-1>=0) {
-                    if(matrix[i-1][j]<matrix[i][j]) max = Math.max(max, dp[i-1][j]);
-                }
-                if(i+1<matrix.length) {
-                    if(matrix[i+1][j]<matrix[i][j]) max = Math.max(max, dp[i+1][j]);
-                }
-                if(j-1>=0) {
-                    if(matrix[i][j-1]<matrix[i][j]) max = Math.max(max, dp[i][j-1]);
-                }
-                if(j+1<matrix[0].length) {
-                    if(matrix[i][j+1]<matrix[i][j]) max = Math.max(max, dp[i][j+1]);
-                }
-                dp[i][j] = Math.max(max+1, dp[i][j]);
+        for(int i=0;i<dp.length;i++) {
+            for(int j=0;j<dp[0].length;j++) {
                 if(res<dp[i][j]) res = dp[i][j];
             }
         }
         return res;
     }
+    public void recall(int[][] dp, int [][] matrix, int i, int j) {
+        if (i - 1 >= 0&&matrix[i-1][j]>matrix[i][j]) {
+            if(dp[i-1][j]==1) {
+                recall(dp,matrix,i-1,j);
+            }
+            dp[i][j] = Math.max(dp[i-1][j]+1,dp[i][j]);
+        }
+        if (j - 1 >= 0&&matrix[i][j-1]>matrix[i][j]) {
+            if(dp[i][j-1]==1) {
+                recall(dp,matrix,i,j-1);
+            }
+            dp[i][j] = Math.max(dp[i][j-1]+1,dp[i][j]);
+        }
+        if (i + 1 < matrix.length&&matrix[i+1][j]>matrix[i][j]) {
+            if(dp[i+1][j]==1) {
+                recall(dp,matrix,i+1,j);
+            }
+            dp[i][j] = Math.max(dp[i+1][j]+1,dp[i][j]);
+        }
+        if (j + 1 < matrix[0].length&&matrix[i][j+1]>matrix[i][j]) {
+            if(dp[i][j+1]==1) {
+                recall(dp,matrix,i,j+1);
+            }
+            dp[i][j] = Math.max(dp[i][j+1]+1,dp[i][j]);
+        }
+    }
+
 
 }
