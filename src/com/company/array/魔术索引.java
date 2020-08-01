5b5a88250d6acce3ef39cd773271c96a353253c6
sheen <sheen_gyx@163.com>
2020-08-02 00:45:24 +0800

    2020 08.01 23:00

---
+++ src/com/company/array/魔术索引.java
@@ -0,0 +1,10 @@
+package com.company.array;
+
+public class 魔术索引 {
+    public int findMagicIndex(int[] nums) {
+        for(int i=0;i<nums.length;i++) {
+            if(i==nums[i]) return i;
+        }
+        return -1;
+    }
+}
--- src/com/company/util/ArrayUtils.java
+++ src/com/company/util/ArrayUtils.java
@@ -77,7 +77,7 @@ public class ArrayUtils {
 
     public static void print2(boolean[][] array) {
         for(int i=0;i<array.length;i++) {
-            for(int j=0;j<array.length;j++) {
+            for(int j=0;j<array[i].length;j++) {
                 printObject(array[i][j]);
             }
             System.out.println();
