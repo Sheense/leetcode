024800ce80f3d39f21ece6d7e9d297db0c1a2c4c
sheen <sheen_gyx@163.com>
2020-08-09 01:25:49 +0800

    2020 08.08 23:00

--- src/com/company/map/判断二分图.java
+++ src/com/company/map/判断二分图.java
@@ -11,16 +11,20 @@ public class 判断二分图 {
     public boolean isBipartite(int[][] graph) {
         if(graph.length==0||graph.length==1) return true;
         int[] judge = new int[graph.length];
-        int index = 0;
+        /*int index = 0;
         while(index<graph.length-1&&graph[index].length==0) {
             index++;
-        }
-        judge[index] = 1;
+        }*/
         Set<Integer> set = new HashSet<>();
-        for(int i = index;i<graph.length;i++) {
-            if(!set.contains(i)) {
-                if(!dfs(judge,graph,index, set)) return false;
+        for(int i = 0;i<graph.length;i++) {
+            if(graph[i].length==0) {
+                continue;
             }
+            if(!set.contains(i)) {
+                judge[i] = 1;
+                if(!dfs(judge,graph,i, set)) return false;
+            }
+
         }
         return true;
     }
@@ -43,8 +47,6 @@ public class 判断二分图 {
 
             }
         }
-
-
         return true;
     }
 }
--- src/com/company/map/概率最大的路径.java
+++ src/com/company/map/概率最大的路径.java
@@ -5,69 +5,55 @@ import java.util.*;
 public class 概率最大的路径 {
     public static void main(String[] args) {
         概率最大的路径 s = new 概率最大的路径();
-        System.out.println(s.maxProbability(3,new int[][]{{0,1}},new double[]{0.5,0.5,0.3},0,2));
+        System.out.println(s.maxProbability(3,new int[][]{{0,1},{1,2},{0,2}},new double[]{0.5,0.5,0.3},0,2));
     }
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        Map<Integer, Set<Integer>> elem = new HashMap<>();
-        double[][] map = new double[n][n];
-        for(int i= 0;i<edges.length;i++) {
-            map[edges[i][0]][edges[i][1]] = succProb[i];
-            map[edges[i][1]][edges[i][0]] = succProb[i];
-            elem.computeIfAbsent(edges[i][0],x->new HashSet<>()).add(edges[i][1]);
-            elem.computeIfAbsent(edges[i][1],x->new HashSet<>()).add(edges[i][0]);
-        }
-        Map<Integer, List<Integer>> elem1 = new HashMap<>();
-        for(int key:elem.keySet()) {
-            List<Integer> list = new ArrayList<>(elem.get(key));
-            sort(list,map,key,0,list.size()-1);
-            elem1.put(key,list);
-        }
+        Queue<Integer> queueLine = new LinkedList<>();
+        Queue<Double> queueScore = new LinkedList<>();
+        Map<Integer, Map<Integer, Double>> map = new HashMap<>();
+        double [] scores = new double[n];
         Set<Integer> set = new HashSet<>();
-        set.add(start);
-        return find(map,start,end,1,elem,set);
-
-    }
-
-    public void sort(List<Integer> list, double[][] map,int begin, int start, int end) {
-        if(start>=end) return;
-        int left = start;
-        int right = end;
-        int key = left;
-        int keyValue = list.get(left);
-        while(left<right) {
-            while(left!=right&&map[begin][list.get(key)]>=map[begin][list.get(right)]) right--;
-            list.set(key, list.get(right));
-            key = right;
-            while(left!=right&&map[begin][list.get(key)]<=map[begin][list.get(left)]) left++;
-            list.set(key,list.get(left));
-            key = left;
+        for(int i=0;i<edges.length;i++) {
+            int pre = edges[i][0];
+            int last = edges[i][1];
+            Map<Integer, Double> map1 = map.computeIfAbsent(pre, x -> new HashMap<>());
+            Map<Integer, Double> map2 = map.computeIfAbsent(last, x -> new HashMap<>());
+            map1.put(last, succProb[i]);
+            map2.put(pre, succProb[i]);
         }
-        list.set(key, keyValue);
-        sort(list,map,begin,start,left-1);
-        sort(list,map,begin,left+1,end);
-    }
-
-    public double find(double[][] map, int start, int end, double res, Map<Integer,Set<Integer>> elem, Set<Integer> set){
-        double max = 0;
-        if(elem.containsKey(start)) {
-            Set<Integer> list = elem.get(start);
-            for(int x:list) {
-                if(set.contains(x)) {
-                    continue;
+        double res = 0;
+        if(!map.containsKey(start)||!map.containsKey(end)) {
+            return res;
+        }
+        queueLine.offer(start);
+        queueScore.offer(1.0);
+        while(!queueLine.isEmpty()) {
+            int size = queueLine.size();
+            boolean flag = false;
+            while(size-->0) {
+                int point = queueLine.poll();
+                double score = queueScore.poll();
+                //保留遍历该点的最大分数
+                if((!set.contains(point)||score>scores[point])&&point!=end) {
+                    scores[point] = score;
+                    set.add(point);
+                    for (Integer i : map.get(point).keySet()) {
+                        double newScore = score*map.get(point).get(i);
+                        queueLine.offer(i);
+                        queueScore.offer(newScore);
+                    }
+                }else if(point==end) {
+                    if(score>res) {
+                        res = score;
+                    }
                 }
-                double v = res*map[start][x];
-                if(v<max) break;
-                if(x==end) {
-                    if(max<v) max=v;
-                    break;
-                }
-                set.add(x);
-                double q = find(map, x,end,v,elem,set);
-                set.remove(x);
-                if(q>max) max = q;
             }
         }
-        return max;
+        return res;
     }
+
+
+
+
 
 }
